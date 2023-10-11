e402d617d9a0403f3f44cc6b6e9822786c1639d2
lovers2020 <dkrkskswl@naver.com>
2023-10-12 00:07:03 +0900

    Update file

--- js/index.js
+++ js/index.js
@@ -14,6 +14,7 @@ searchInputEl.addEventListener("blur", function () {
 });
 
 const badgeEl = document.querySelector("header .badges");
+const toTopEl = document.getElementById("to-top");
 
 window.addEventListener(
   "scroll",
@@ -24,15 +25,30 @@ window.addEventListener(
         opacity: 0,
         display: "none",
       });
+      // 버튼 보이기
+      gsap.to("#to-top", 0.2, {
+        x: 0,
+      });
     } else {
+      //배지 보이기
       gsap.to(badgeEl, 0.6, {
         opacity: 1,
         display: "block",
+      });
+      // 버튼 숨기기
+      gsap.to(toTopEl, 0.2, {
+        x: 100,
       });
     }
   }, 300)
 );
 // _.throttle(함수, 시간 {
+
+toTopEl.addEventListener("click", function () {
+  gsap.to(window, 0.7, {
+    scrollTo: 0,
+  });
+});
 
 const fadeEls = document.querySelectorAll(".visual .fade-in");
 fadeEls.forEach(function (fadeEl, index) {
@@ -122,3 +138,6 @@ spyEls.forEach(function (spyEl) {
     .setClassToggle(spyEl, "show")
     .addTo(new ScrollMagic.Controller());
 });
+
+const thisYear = document.querySelectorAll(".this-year");
+thisYear.textContent = new Date().getFullYear();
