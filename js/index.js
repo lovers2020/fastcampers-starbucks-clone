7730fc07d7f86da69604fd2c245ddc4117816d65
lovers2020 <dkrkskswl@naver.com>
2023-10-12 13:56:18 +0900

    Seperate common file

--- js/index.js
+++ js/index.js
@@ -1,17 +1,3 @@
-const searchEl = document.querySelector(".search");
-const searchInputEl = searchEl.querySelector("input");
-
-searchEl.addEventListener("click", function () {
-  searchInputEl.focus();
-});
-searchInputEl.addEventListener("focus", function () {
-  searchEl.classList.add("focused");
-  searchInputEl.setAttribute("placeholder", "통합 검색");
-});
-searchInputEl.addEventListener("blur", function () {
-  searchEl.classList.remove("focused");
-  searchInputEl.setAttribute("placeholder", "");
-});
 
 const badgeEl = document.querySelector("header .badges");
 const toTopEl = document.getElementById("to-top");
@@ -138,6 +124,3 @@ spyEls.forEach(function (spyEl) {
     .setClassToggle(spyEl, "show")
     .addTo(new ScrollMagic.Controller());
 });
-
-const thisYear = document.querySelectorAll(".this-year");
-thisYear.textContent = new Date().getFullYear();
